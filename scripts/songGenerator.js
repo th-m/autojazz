ac7fe8c3a035bdf463c4edf14d97eb9e6cb1e717
thom <thom@MacBook-Air.local>
2017-08-09 06:42:14 -0600

    slight clean up

--- scripts/songGenerator.js
+++ scripts/songGenerator.js
@@ -29,50 +29,13 @@ const theory = {
       '11': "Dim"
   }
 }
-// const 
-// 
-// const scale = ['1','3','5','6','8','10','12'];
-// 
-// const modes = {
-//     '1'= "Ionian",
-//     '3'= "Dorian",
-//     '5'= "Phrygian",
-//     '6'= "Lydian",
-//     '8'= "Mixolydian",
-//     '10'= "Aeolian",
-//     '12'= "Locrian"
-// }
-// const chords = {
-//     '1'= "Maj",
-//     '3'= "Min",
-//     '5'= "Min",
-//     '6'= "Maj",
-//     '8'= "Maj",
-//     '10'= "Min",
-//     '12'= "Dim"
-// }
 const song = {};
 
 song.getIntDegree = (key,deg) => {
-  // return new Promise((resolve, reject) =>{
-    // console.log(key.toUpperCase());
-    // console.log(deg);
-    // console.log(theory.notes);
-    // console.log(theory.majScale);
     let i = theory.notes.indexOf(key.toUpperCase());
     let j = theory.majScale.indexOf(deg.toString());
-    // console.log(i);
-    // console.log(j);
     return theory.notes[i+j];
-    // resolve(theory.notes[i+j]);
-  // });
-  
 }
-
-
-// Promise.all([getSongMelody(req.params.note), getSongChords(req.params.note)]).then(values => { 
-//   res.render('mc/index', { title: 'MC', songData: values, note: req.params.note});
-// });
 
 song.readFile = (filePath) => {
   return new Promise((resolve, reject) =>{
@@ -86,15 +49,7 @@ song.readFile = (filePath) => {
           }
       });
       console.log(File.ChannelEvent);
-      // resolve(File.ChannelEvent);
-      // resolve(file.ChannelEvent);
-      // file.addTrack(1, // position (optional) 
-      //     new File.ChannelEvent(File.ChannelEvent.TYPE.NOTE_ON, {
-      //         note: 45
-      //     }),
-      //     new File.MetaEvent(File.MetaEvent.TYPE.END_OF_TRACK)
-      // );
-      // console.log(File);
+    
       file.getHeader().setTicksPerBeat(80);
       file.getData(function (err, data) {
           if (err) {
@@ -106,87 +61,15 @@ song.readFile = (filePath) => {
                   throw err;
               }
               
-              // file at 'path' now contains binary MIDI data 
-              // ready to be played by any other MIDI program 
-              // (or re-read by this module later) 
+            
           });
       });
-      // file.addTrack(1, // position (optional) 
-      //     new File.ChannelEvent(File.ChannelEvent.TYPE.NOTE_ON, {
-      //         note: 45
-      //     }),
-      //     new File.MetaEvent(File.MetaEvent.TYPE.END_OF_TRACK)
-      // );
-      // console.log(file.getHeader());
-      // console.log(file.getTracks());
       resolve(file);
     });
     
   });
 }
-// 
-// song.genDrumHiHat = (tone) => {
-//   return new Promise((resolve, reject) => {
-//       let melody = scribble.clip({
-//           notes: ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3'], // this works too ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3']
-//           pattern: 'x-x-x-x-'.repeat(8)
-//       });
-//       resolve(melody);
-//   });
-// }
-// song.genDrumSnare = (tone) => {
-//   return new Promise((resolve, reject) => {
-//       let melody = scribble.clip({
-//           notes: ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3'], // this works too ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3']
-//           pattern: 'x-x-x-x-'.repeat(8)
-//       });
-//       resolve(melody);
-//   });
-// }
-// song.genDrumKick = (tone) => {
-//   return new Promise((resolve, reject) => {
-//       let melody = scribble.clip({
-//           notes: ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3'], // this works too ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3']
-//           pattern: 'x-x-x-x-'.repeat(8)
-//       });
-//       resolve(melody);
-//   });
-// }
-// song.genSongMelody = (tone) => {
-//   return new Promise((resolve, reject) => {
-//       let melody = scribble.clip({
-//           notes: ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3'], // this works too ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3']
-//           pattern: 'x-x-x-x-'.repeat(8)
-//       });
-//       resolve(melody);
-//   });
-// }
-// song.genSongChords = (tone) => {
-//   return new Promise((resolve, reject) => {
-//       let chords = scribble.clip({
-//         notes: ['F#min', 'C#min', 'Dmaj', 'Bmin', 'Emaj', 'Amaj', 'Dmaj', 'C#min', 'Amaj'],
-//         pattern: 'x_x_x_--'.repeat(8),
-//         sizzle: true
-//       });  
-//       resolve(chords);
-//   });
-// }
-// song.genInsScribble = (tone) => {
-//   return new Promise((resolve, reject) => {
-//       let chords = scribble.clip({
-//         notes: ['F#min', 'C#min', 'Dmaj', 'Bmin', 'Emaj', 'Amaj', 'Dmaj', 'C#min', 'Amaj'],
-//         pattern: 'x_x_x_--'.repeat(8),
-//         sizzle: true
-//       });  
-//       resolve(chords);
-//   });
-// }
-// song.getIntDegree = (key,deg) => {
-// song.loop = (num) => {
-//   for (var i=0;i<n;i++) {
-//     console.log(array[i])
-//   }
-// }
+
 song.scribbleInstrument = (key,instrument) => {
   // return new Promise((resolve, reject) => {
     let scribNotes = [];
